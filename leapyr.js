1e85f4981f4c59408fc0bbdaf7734fc54fd85b72
Saeed Ahmed <sahmed@c-cor.local>
2021-07-16 10:21:34 +1000

    adding leapyr.js

---
+++ leapyr.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
+function leapYear (yr){
+    if (yr%4===0)
+    console.log(yr + " "+"leap year")
+  else 
+  console.log(yr +" "+"not a leap year");
+
+return yr;
+
+}
+console.log(leapYear (2008));
